e793a8c865d5dce35d721dc91ad23e75acc92781
Aitanacm <86914128+Aitanacm@users.noreply.github.com>
2022-06-07 09:37:54 +0200

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-let fondoActual = "url(img/isla-pokemon.png) 0% 0% / cover no-repeat";
-let modoActual = "bg-light text-dark";
-let num = Math.floor((Math.random() * (898 - 1 + 1)) + 1);
-Pokemon.getPokemon(num).then(pokemon => pokemon.pintaPokemon())
